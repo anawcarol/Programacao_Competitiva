221a2c9d43c6be0738acebfcfbf6ba5f3b3173a8
Ana Carolina Madeira Fialho <01009547933@semob.df.gov.br>
2025-10-15 12:01:28 -0300

    pilha exercicio

--- EDA/pilha/zero.cpp
+++ EDA/pilha/zero.cpp
@@ -3,7 +3,33 @@ using namespace std;
 
 
 int main() {
+    int n;
+    cin >> n;
+
+    stack<int> pilha;
+    for (int i = 0; i < n; i++)
+    {
+        int valor;
+        cin >> valor;
+        pilha.push(valor);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (pilha.top() == 0)
+        {
+            pilha.pop();
+        }
+    }
     
+    int soma = 0;
+    while (!pilha.empty()){
+        
+        soma += pilha.top();
+        pilha.pop();
+    }
+
+    cout << soma << endl;
 
     return 0;
 }
