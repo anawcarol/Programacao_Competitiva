ebb79f9071023a56961ccf5197b8f8503fbd5746
Ana Carolina <anacarolinamfialho@gmail.com>
2026-03-20 14:48:33 -0300

    questoes

---
+++ questoes/clube.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n, l, k;
+
+    while (m > 0 && n > 0)
+    {
+        
+        cin >> m >> n >> l >> k;
+        vector<int> x;
+
+        for (int i = 0; i < k; i++)
+        {
+            int a;
+            cin >> a;
+            x.push_back(a);
+        }
+    }
+
+    return 0;
+}
