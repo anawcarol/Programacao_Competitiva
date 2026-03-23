21048c98d0617bb099024612093054041ee2d15a
anawcarol <anacarolinamfialho@email.com>
2026-03-23 18:05:15 -0300

    contest 1

--- Contest/2026.1/constest1/b.cpp
+++ Contest/2026.1/constest1/b.cpp
@@ -4,9 +4,40 @@ using namespace std;
 int main()
 {
     int n;
+    cin >> n;
     vector<int> a;
+    vector<int> resposta;
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int x = sqrt(a[i]);
+        if (x * x != a[i]){
+            resposta.push_back(a[i]);
+        }
+        
+    }
+
+    int maior = resposta[0]; //precisa ser assim, pois numeros negativos e o maior numero pode ser negativo, entao menor do que zero, entao nao pode ser zero
+
+    for (int i = 0; i < resposta.size(); i++)
+    {
+        
+        if (resposta[i] > maior)
+        {
+            maior = resposta[i];
+        }
+       
+    }
+
+
+    cout << maior << endl;
 
     return 0;
 }
